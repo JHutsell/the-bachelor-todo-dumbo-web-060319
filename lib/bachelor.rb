9ae666ace4acd5db948163bf30d24fdd2ef60592
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 22:44:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,5 +51,5 @@ def get_average_age_for_season(data, season)
     age_total += (contestant_hash["age"]).to_i
     contestants += 1
   end
-  age_total / contestants.to_f
+  (age_total / contestants.to_f).round
 end
